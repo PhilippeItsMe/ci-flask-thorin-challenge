2c29e06b9c222ab1bad5edb8116c1424e13992c0
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2024-10-29 13:23:37 +0000

    Debuging on going

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
 import os
-#To import the class Flask for the file flask
-#To import the function render_template for the file flask
 from flask import Flask, render_template
 
-#To crate a instance of the class Flask
+
 app = Flask(__name__)
 
-#To create different route the the menu
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -22,9 +20,9 @@ def contact():
     return render_template("contact.html")
 
 
-@app.route("/carreer")
-def carreer():
-    return render_template("carreer.html")
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
 
 
 if __name__ == "__main__":
