b1c6e7e0b63f68b3b527b899a05aed770b50ea28
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2024-10-29 10:14:55 +0000

    Menu template set-up

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 import os
+#To import the class Flask for the file flask
+#To import the function render_template for the file flask
 from flask import Flask, render_template
 
-
+#To crate a instance of the class Flask
 app = Flask(__name__)
 
-
+#To create different route the the menu
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -18,6 +20,11 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/carreer")
+def carreer():
+    return render_template("carreer.html")
 
 
 if __name__ == "__main__":
